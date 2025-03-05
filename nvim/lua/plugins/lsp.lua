83e7de4011e58b6bc46bdacae708f671cee8be7f
Marius Aga <mnaga@stud.ntnu.no>
2025-03-05 09:57:58 +0100

    Updated lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,57 +1,62 @@
 return {
-    "VonHeikemen/lsp-zero.nvim",
+  "VonHeikemen/lsp-zero.nvim",
 
-    dependencies = {
-        { "neovim/nvim-lspconfig" }, -- Required
-        { "williamboman/mason.nvim" },
-        { "williamboman/mason-lspconfig.nvim" }, -- Optional
-        { "WhoIsSethDaniel/mason-tool-installer.nvim" },
-        -- Autocompletion
-        { "hrsh7th/nvim-cmp" }, -- Required
-        { "hrsh7th/cmp-nvim-lsp" }, -- Required
-        { "L3MON4D3/LuaSnip" }, -- Required
-    },
+  dependencies = {
+    { "neovim/nvim-lspconfig" }, -- Required
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" }, -- Optional
+    { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+    -- Autocompletion
+    { "hrsh7th/nvim-cmp" }, -- Required
+    { "hrsh7th/cmp-nvim-lsp" }, -- Required
+    { "L3MON4D3/LuaSnip" }, -- Required
+  },
 
-    config = function()
+  config = function()
 
-        require("mason").setup()
+    require("mason").setup()
 
-        local lsp = require("lsp-zero")
-        local mason_tool_installer = require("mason-tool-installer")
-        local mason_lspconfig = require("mason-lspconfig")
+    local lsp = require("lsp-zero")
+    local mason_tool_installer = require("mason-tool-installer")
+    local mason_lspconfig = require("mason-lspconfig")
+    local lspconfig = require("lspconfig")
 
-        lsp.on_attach(function(client, bufnr)
-            local opts = {buffer = bufnr, remap = false}
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end)
+    lsp.on_attach(function(client, bufnr)
+      local opts = {buffer = bufnr, remap = false}
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    end)
 
-        lsp.omnifunc.setup({
-            tabcomplete = true,
-            use_fallback = true,
-            update_on_delete = true,
-        })
+    lsp.omnifunc.setup({
+      tabcomplete = true,
+      use_fallback = true,
+      update_on_delete = true,
+    })
 
+    -- List of LSP servers
+    local lsps = {
+      "lua_ls",
+      "pyright",
+      "yamlls",
+      "clangd",
+      "gopls",
+      "ts_ls",
+      "rust_analyzer",
+      "jsonls",
+      "html",
+      "cssls",
+    }
 
-        mason_lspconfig.setup({
-          ensure_installed = {
-            "lua_ls",
-            "pyright",
-            "yamlls",
-            "clangd",
-            "ts_ls",
-          },
-          opts = {
-            auto_install = true,
-          }
-      })
-
+    mason_lspconfig.setup({
+      ensure_installed = lsps,
+      automatic_installation = true,
+    })
 
     mason_tool_installer.setup({
       ensure_installed = {
@@ -62,25 +67,14 @@ return {
       }
     })
 
-    require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-    --[ require("lspconfig").lua_ls.setup({}) --]
+    -- Auto-install and configure LSPs
+    for _, server_name in ipairs(lsps) do
+      mason_lspconfig.setup_handlers({
+        function()
+          lspconfig[server_name].setup({})
+        end
+      })
+    end
 
-    require("lspconfig").clangd.setup({})
-    require("lspconfig").ts_ls.setup({})
-    require("lspconfig").pyright.setup({
-      filetypes = { "python" },
-    })
-    require("lspconfig").yamlls.setup({
-            settings = {
-                yaml = {
-                    format = {
-                        enable = false,
-                    },
-                    validate = true,
-                    completion = true,
-                    hover = true,
-                },
-            },
-        })
   end
 }

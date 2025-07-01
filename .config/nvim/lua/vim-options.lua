f34e08c64be3a751d9d3a4b567304ab308d4b8a8
Marius Aga <mnaga@stud.ntnu.no>
2025-07-02 00:23:00 +0200

    Modularized init.lua, removed floatterminal for snacks terminal

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -23,3 +23,15 @@ vim.opt.listchars = {
 	precedes = "«",
 	leadmultispace = "│" .. string.rep(" ", vim.opt.shiftwidth:get() - 1),
 }
+
+-- Sets colors for cursorLine, all other lines and visual highlight
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFFFFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" }) -- Remove background from the entire line
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#DEAFAE", bold = false })
+vim.api.nvim_set_hl(0, "Visual", { bg = "#515051", fg = "NONE" })
+
+-- Auto reload aerospace on save
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "aerospace.toml" },
+	command = "!aerospace reload-config",
+})
